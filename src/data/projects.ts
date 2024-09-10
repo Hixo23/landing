05a620202eb770222f8330b9a7b6b2675208de95
Hixo <hixohixo32@gmail.com>
2024-09-10 16:49:02 +0200

    feat: add projects section

---
+++ src/data/projects.ts
@@ -0,0 +1,54 @@
+export const Projects = [
+  {
+    title: "Jobguru",
+    description:
+      "Jobguru to serwis z ofertami pracy. Strona umożliwia użytkownikami autoryzacje, dodawanie ofert oraz przeglądanie ich",
+    technologies: ["Vue", "Symfony", "TailwindCSS", "Typescript", "Vite"],
+    demo: "",
+    repositories: [
+      {
+        name: "Client",
+        url: "https://github.com/Hixo23/JobGuru-Client",
+      },
+      {
+        name: "Server",
+        url: "https://github.com/Hixo23/JobGuru---Server",
+      },
+    ],
+    image: "https://i.imgur.com/oHMT9Cm.png",
+  },
+  {
+    title: "PollPal",
+    description:
+      "Pollpal to przyjazna dla użytkownika aplikacja do tworzenia ankiet.",
+    technologies: [
+      "Next.JS",
+      "TailwindCSS",
+      "Typescript",
+      "React",
+      "MongoDB",
+      "Next Auth",
+    ],
+    repositories: [
+      {
+        name: "",
+        url: "https://github.com/Hixo23/poll-app",
+      },
+    ],
+    image: "https://i.imgur.com/3Mm5RqM.png",
+    demo: "https://poll-app-topaz.vercel.app",
+  },
+  {
+    title: "Devfinder",
+    description: "Prosta aplikacja do znajdowania statystyk kont github",
+    technologies: ["React", "Typescript", "TailwindCSS", "Vite"],
+    image: "https://i.imgur.com/qn4UiqG.gif",
+    demo: "https://clever-babka-94dcc8.netlify.app/",
+    repositories: [
+      {
+        name: "",
+        url: "https://github.com/Hixo23/devfinder",
+      },
+    ],
+  },
+];
